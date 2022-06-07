da0500b68ce0f268a76005c5969fc0f80e565ff6
Drael Davidson <drael_davidson@hotmail.com>
2022-06-06 23:27:52 -0700

    done with fundementals

--- Chapter 4 Javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
+++ Chapter 4 Javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/assignment.js
@@ -25,7 +25,7 @@
 // return (population / 7900)*100;
 // }
 // console.log("FUNCTIONS DECLARATIONS");
-// console.log(percentageOfWorld(1441));
+// console.log(World(1441));
 // console.log(percentageOfWorld(332));
 // console.log(percentageOfWorld(47));
 
@@ -57,28 +57,109 @@
 // console.log(describePopulation("China", 1441));
 
 //ARRAYS
+// const populations = [1441, 332, 47, 33];
+
+// const arraySize = arrayNew => arrayNew.length === 4 ? 'true' : 'false';
+// console.log(arraySize(populations));
+
+// console.log(percentageOfWorld(populations[0]),percentageOfWorld(populations[1]),percentageOfWorld(populations[2]),percentageOfWorld(populations[3]));
 
 
+//Basic Array Operations (Methods)
+// const neighbours = ['Canada', 'Mexico', 'Greenland']
+// console.log(neighbours);
+
+// neighbours.push('Utopia');
+// console.log(neighbours);
+
+// neighbours.pop();
+// console.log(neighbours);
+
+// if (!neighbours.includes('Germany')){
+//     console.log( 'Probably not a central European country :D');
+// }
+
+// console.log(neighbours.indexOf('Greenland'));
+// neighbours[2] = 'The Greenest of Lands'
+// console.log(neighbours);
 
 
+//OBJECTS
+// const myCountry = {
+//     country: 'United States of America',
+//     capital: 'Washington DC',
+//     language: 'English', 
+//     population: 332,
+//     neighbours: ['Canada', 'Mexico', 'Utopia'],
+//     isIsland: false,
 
+//OBJECT METHOD
+//     discribe: function () {
+//         return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.isIsland? 'is an island' : 'is not an island'}, & ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.'`
+//     },
 
+// }
+// // console.log(myCountry);
 
+// console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.'`);
 
+// console.log(myCountry.population + 2);
+// console.log(myCountry['population'] - 2);
 
+// console.log(myCountry.discribe());
 
+//for loop
 
+// for (let voter = 1; voter <= 50; voter++) {
+//     console.log(`Voter number ${voter} is currently voting`);
+    
+// }
 
+//Looping Arrays, Breaking and Continuing
 
+// const populations = [1441, 332, 47, 33];
+// const percentages2 = [];
 
+// for (let row = 0; row < populations.length; row++) {
+//     percentages2.push((populations[row] / 7900)*100);
 
+//     // console.log(percentages2);
+    
+// }
+// console.log(percentages2);
 
+//loop in loop 
 
+// const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+// const newArray = [];
 
+// for (let row = 0; row < listOfNeighbours.length; row++) {
+//     // console.log(listOfNeighbours[row]);
+//     for (let neighbour = 0; neighbour < listOfNeighbours[row].length; neighbour++) {
+//         const countryNeighbor = listOfNeighbours[row][neighbour]
+//         console.log(`Neighbour: ${countryNeighbor}`);
+        
+//     }
+// }
 
+//WHILE LOOP
 
+// const populations = [1441, 332, 47, 33];
+// const percentages2 = [];
 
+// // for (let row = 0; row < populations.length; row++) {
+// //     percentages2.push((populations[row] / 7900)*100);
 
+// //     // console.log(percentages2);
+    
+// // }
+// // console.log(percentages2);
+// let row = 0;
+// while (row < populations.length) {
+//     percentages2.push(((populations[row] / 7900)*100));
+//     row++
+// }
+// console.log(percentages2);
 
 
 
@@ -136,3 +217,169 @@ GOOD LUCK 😀
 // }
 // console.log(scoreDolphins,scoreKoalas);
 // checkWinner(scoreDolphins, scoreKoalas);
+
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+
+GOOD LUCK 😀
+*/
+
+// const calcTip = function(bill){
+//     if (bill >= 50 && bill <= 300){
+//        return (bill * .15);
+//     }else{
+//         return (bill * .20);
+
+//     }
+
+// }
+// console.log(calcTip(500));
+
+// const bills = [125, 555, 44];
+// console.log(`The bills are: ${bills}`);
+
+
+// const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
+// console.log(`The tips will be: ${tips}`);
+
+// const total = [(bills[0]+tips[0]),(bills[1]+tips[1]),(bills[2]+tips[2])];
+
+// // const total = [(bills[0]+calcTip(bills[0])),(bills[1]+calcTip(bills[1])),(bills[2]+calcTip(bills[2]))];
+// console.log(`The totals will be: ${total}`);
+
+
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! 
+Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+*/
+
+
+// const mark = {
+//     fullName: "Mark Miller",
+//     mass: 78,
+//     height: 1.69,
+
+//     calcBMI: function () {
+//         this.bmi = this.mass / this.height ** 2;
+//         return this.bmi
+//     },
+// }
+
+// const john = {
+//     fullName: "John Smith",
+//     mass: 92,
+//     height: 1.95,
+
+//     calcBMI: function () {
+//         this.bmi = this.mass / this.height ** 2;
+//         return this.bmi
+//     },
+
+// }
+// // mark.calcBMI();
+// // john.calcBMI();
+// // console.log(mark.bmi, john.bmi);
+
+// if (mark.calcBMI()> john.calcBMI()){
+//     console.log(`${mark.fullName} has the higher BMI: ${mark.calcBMI()} vs. ${john.calcBMI()}`);
+// }else{
+//     console.log(`${john.fullName} has the higher BMI: ${john.calcBMI()} vs. ${mark.calcBMI()}`);
+
+// }
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+*/
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let row = 0; row < bills.length; row++) {
+    if (bills[row] >= 50 && bills[row] <= 300){
+        // console.log(bills[row] * .15);
+        const tip =bills[row] * .15;
+        // tips.push(bills[row] * .15);
+        tips.push(tip);
+        // totals.push((bills[row])+(bills[row] * .15))
+        const total = (bills[row])+tip;
+        totals.push(total);
+
+        console.log(`The $${bills[row]} will have a ${tip} tip and a $${total} total`);
+
+    }else{
+        // console.log(bills[row] * .20);
+        const tip =bills[row] * .20;
+        // tips.push(bills[row] * .20);
+        tips.push(tip);
+        // totals.push((bills[row])+(bills[row] * .20))
+        // totals.push((bills[row])+tip);
+        const total = (bills[row])+tip;
+        totals.push(total);
+
+        console.log(`The $${bills[row]} will have a ${tip} tip and a $${total} total`);
+
+}
+}
+// console.log(bills);
+// console.log(tips);
+// console.log(totals);
+
+// 4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+//   4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+//   4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+//   4.3. Call the function with the 'totals' array
+const arr = [];
+const calcAverage = function(arr){
+    let sum = 0;
+    for (let row = 0; row < arr.length; row++) {
+         sum += arr[row];
+    }
+    const average = sum / arr.length
+    return average;
+
+}
+console.log(calcAverage(totals));
--- Chapter 4 Javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Chapter 4 Javascript/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -43,22 +43,261 @@
 // console.log(calcAge3(1992));
 
 //FUNCTION CALLING OTHER FUNCTIONS
-function cutFruitPieces(fruit){
-    return fruit * 3;
-}
+// function cutFruitPieces(fruit){
+//     return fruit * 3;
+// }
 
 
 
 
-function fruitProccessor(apple, oranges) {
-    const applePieces =cutFruitPieces(apple);
-    const orangePieces =cutFruitPieces(oranges);
+// function fruitProccessor(apple, oranges) {
+//     const applePieces =cutFruitPieces(apple);
+//     const orangePieces =cutFruitPieces(oranges);
 
 
-    // const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+//     // const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
   
-    return juice;
-}
+//     return juice;
+// }
 
-console.log(fruitProccessor(2,3));
+// console.log(fruitProccessor(2,3));
+
+//ARRAYS
+// const friend1 = 'Michael';
+// const friend2 = 'Steven';
+// const friend3 = 'Peter';
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+// console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+// console.log(friends[0]);
+// console.log(friends.length);
+// console.log(friends[friends.length-1]);
+
+// friends[2] = 'Jay';
+// console.log(friends);
+
+// const firstName = 'Drael';
+// const drael = [firstName, 'Davidson', 2022-1992, 'developer', friends];
+// console.log(drael);
+
+// const calcAge = function (birthYear){
+//     return 2022- birthYear;
+// }
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[1])
+// const age2 = calcAge(years[2])
+// const age3 = calcAge(years[3])
+
+
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[1]), calcAge(years[2]), calcAge(years[3]) ];
+//     console.log(ages);
+
+//     friends.push('Hannah');
+//     console.log(friends);
+
+//     const unshifted = friends.unshift('John');
+//     console.log(unshifted);
+//     console.log(friends);
+
+//     friends.pop();
+//     console.log(friends);
+
+//     console.log(friends.indexOf('Peter'));
+//     console.log(friends.includes('Bob'));
+    
+//     friends[3]='Petersburg';
+//     console.log(friends);
+
+
+//Objects
+
+// const drael = {
+//     firstName: 'Drael',
+//     lastName: 'Davidson',
+//     age: 2022-1992,
+//     job: 'developer',
+//     friends: ['Shelly','Lauren','Drake']
+// };
+
+// console.log(drael);
+// console.log(drael.lastName);
+// console.log(drael['lastName']);
+
+// const nameKey = 'Name';
+// console.log(drael['first'+ nameKey]);
+// console.log(drael['last'+ nameKey]);
+
+// const interestedIn = prompt('What do you want to know about Drael? Choose between firstName, lastName, age, job, and friends');
+
+// console.log(drael[interestedIn]);
+// if (drael[interestedIn]){
+//     console.log(drael[interestedIn]);
+// }else{
+//     console.log('Wrong request. Try again');
+// }
+
+// drael.location = 'USA';
+// drael['twitter']= '@drizzleonrice';
+// console.log(drael);
+
+//CHALLENGE 
+//Drael has 3 friends and her best friend is 'Shelly'
+// console.log(`${drael.firstName} has ${drael.friends.length} friends and her best friend is '${drael.friends[0]}!'`);
+
+//METHODS
+// const drael = {
+//     firstName: 'Drael',
+//     lastName: 'Davidson',
+//     birthYear: 1992,
+//     job: 'developer',
+//     friends: ['Shelly','Lauren','Drake'], 
+//     hasDriversLincense: true,
+
+    // calcAge: function (birthYear) {
+    //     return 2022 - birthYear
+    // }
+
+    // calcAge: function () {
+    //     // console.log(this);
+    //     return 2022 - this.birthYear
+    // }
+
+//     calcAge: function () {
+//         // console.log(this);
+//         this.age = 2022 - this.birthYear;
+//         return this.age;
+//     },
+
+//     // license: function(){
+//     //     if (this.hasDriversLincense === true){
+//     //         this.yesOrNo ='a';
+//     //         return this.yesOrNo;
+//     //     }else{
+//     //         this.yesOrNo ='no';
+//     //         return this.yesOrNo;
+//     //     }
+//     // },
+
+//     getSummary: function () {
+//         // return`${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and she has ${this.license()} driver's license`;
+//         return`${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and she has ${this.hasDriversLincense ? 'a' : 'no'} driver's license`;
+
+//     },
+
+// };
+// console.log(drael.calcAge());
+// console.log(drael['calcAge']());
+
+// console.log(drael.getSummary());
+
+
+//LOOPS
+//FOR LOOP KEEPS RUNNING WHILE CONDITION IS TRUE
+
+// for (let rep = 1; rep <= 100; rep++) {
+//     console.log(`Lifting weights repetition ${rep} 🏋🏾‍♂️`);    
+// }
+
+//LOOPING AN ARRAY
+
+// const draelArray = [
+//     'Drael',
+//     'Davidson',
+//     2022-1992,
+//     'developer',
+//     ['Shelly','Lauren','Drake'],
+//     true,
+// ];
+// const types = [];
+// for (let row = 0; row < draelArray.length; row++) {
+//     console.log(draelArray[row], typeof draelArray[row]);    
+
+//     //FILLING AN ARRAY
+//     // types[row] = typeof draelArray[row]
+//     types.push(typeof draelArray[row]);
+// }   
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let row = 0; row < years.length; row++) {
+//     // console.log(2022 - years[row]); 
+//     ages.push(2022 - years[row])
+// }
+// console.log(ages);
+
+//continue will skip if true and continue the loop
+
+// console.log('----ONLY STRINGS----');
+// for (let row = 0; row < draelArray.length; row++) {
+//     if (typeof draelArray[row] !== 'string')
+//     continue;
+    
+//     console.log(draelArray[row], typeof draelArray[row]);    
+// }
+
+// //break will completly terminate the loop
+// console.log('----BREAK WITH NUMBER----');
+// for (let row = 0; row < draelArray.length; row++) {
+//     if (typeof draelArray[row] === 'number')
+//     break;
+    
+//     console.log(draelArray[row], typeof draelArray[row]);    
+// }
+
+//looping an array backwards
+// const draelArray = [
+//     'Drael',
+//     'Davidson',
+//     2022-1992,
+//     'developer',
+//     ['Shelly','Lauren','Drake'],
+//     true,
+// ];
+
+// for (let row = draelArray.length - 1; row >= 0; row--) {
+//     console.log(row, draelArray[row]);
+    
+// }
+
+//loop in loop 
+// for (let excercise = 1; excercise <= 3; excercise++) {
+//     console.log(`----- STARTING EXCERSICE ${excercise}`);
+//     for (let rep = 1; rep <= 5; rep++) {
+//         console.log(`${rep} REP 🏋🏾‍♂️`);
+        
+//     }
+//     console.log(`---REST FOR 3`);
+
+//     for (let rest = 1; rest <=3; rest++) {
+//         console.log(`😚 INHALE... EXHALE... ${rest}`);
+        
+//     }
+// }
+
+//WHILE LOOP
+// let rep = 1;
+// while (rep <= 10) {
+//          console.log(`${rep} REP 🏋🏾‍♂️`);
+//          rep++
+// }
+
+let dice = Math.trunc(Math.random() * 12) + 1
+// console.log(dice);
+let counter = 0;
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 12) + 1
+    counter++;
+}
+console.log(`YAY!!!! You rolled a ${dice} in ${counter + 1} moves`);
+
+
