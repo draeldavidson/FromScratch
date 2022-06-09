e48afc6356d9c1dd5607341e4493158c7fbc0083
Drael Davidson <drael_davidson@hotmail.com>
2022-06-09 16:15:47 -0700

    array work

--- Chapter 4 Javascript/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ Chapter 4 Javascript/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const { weekdays } = require("moment");
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -16,11 +18,6 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
-    console.log(
-      `Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address}, at ${time}.`
-    );
-  },
   openingHours: {
     thu: {
       open: 12,
@@ -35,7 +32,121 @@ const restaurant = {
       close: 24,
     },
   },
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address}, at ${time}.`
+    );
+  },
+  orderPasta: function (ingr1, ingr2, ingr3) {
+    console.log(
+      `Here is your delicious pasta with ${ingr1}, ${ingr2}, and ${ingr3}.`
+    );
+  },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  }
 };
+
+//ITERATING THROUGH AN ARRAY
+//1
+const menu = [...restaurant.starterMenu, ... restaurant.mainMenu];
+for (const item of menu) {
+  console.log(item);
+  
+}
+//2
+for (const item of menu.entries()) {
+  console.log(`${item[0] + 1}: ${item[1]}`);
+  
+}
+//3
+for (const [item, entries] of menu.entries()) {
+  console.log(`${item + 1}: ${entries}`);
+  
+}
+
+
+
+//////////////// DESTRUCTURING //////////////////////
+//SPREAD VS REST
+
+//SPREAD on the RIGHT SIDE of =
+// const arr = [1,2,...[3,4]];
+// console.log(arr);
+
+// //REST on the LEFT SIDE of =
+// const [a,b, ...others]= [1,2,3,4,5];
+// console.log(a,b, others);
+// const welp = [a,b, ...others];
+// console.log(...welp);
+// //OBJECTS
+// const {sat, ...weekdays} = restaurant.openingHours;
+// console.log(weekdays);
+
+//////////// Functions ///////////////
+// can pass and array or just numbers.
+
+// const add = function (...numbers) {
+//   // console.log(...numbers);
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum+=numbers[i]
+    
+//   }
+// console.log(sum);
+// }
+
+// add(2,3);
+// add(5,3,7,2);
+// add(8,2,5,3,2,1,4);
+// const x = [23,5,7];
+// add(...x);
+
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+// restaurant.orderPizza('mushrooms');
+
+///////////////////////////////////
+// const ingredients = [
+//   prompt("let's make pasta! ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+// restaurant.orderPasta(...ingredients);
+
+//Object
+// const newResturant = {foundedIn: 1998, ...restaurant, founder: 'Guiseppe'};
+// console.log(newResturant);
+
+// const restaurantCopy = {...restaurant};
+// restaurantCopy.name = 'Restoranti Roma';
+// console.log(restaurantCopy.name);
+// console.log(restaurant.name);
+
+
+//SPREAD OPERATOR
+// const arr = [7,8,9];
+// const badNewArr = [1,2,arr[0],arr[1],arr[2]];
+// console.log(badNewArr);
+
+// const newArr = [1,2,...arr];
+// console.log(newArr);
+
+// console.log(...newArr);
+
+// the spread operator is similar to destructuring both help get elements out of an array.
+//the big difference is that ...  takes all the elements from an array and doesnt create new variables
+//we can only use it in places where we find values separated by commas.
+
+//COPY ARRAY
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// console.log(mainMenuCopy);
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT objects
 
 // restaurant.orderDelivery({
 //   time: '22:30',
