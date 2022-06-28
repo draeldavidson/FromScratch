aa79fa8e7cf0c121650d59bcfd5153b0bf086418
Drael Davidson <drael_davidson@hotmail.com>
2022-06-27 18:59:28 -0700

    :|

--- Chapter 4 Javascript/complete-javascript-course-master/15-Mapty/starter/script.js
+++ Chapter 4 Javascript/complete-javascript-course-master/15-Mapty/starter/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+///////////////////////////////////////////////////////
+//☁️                  SELECTORS                      //
+//////////////////////////////////////////////////////
+
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -11,4 +13,317 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition()
+///////////////////////////////////////////////////////
+//☁️           CLASSES AND FUNCTIONS                 //
+//////////////////////////////////////////////////////
+
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  clicks = 0;
+
+  // CONSTRUCTOR
+  constructor(coords, distance, duration) {
+    this.coords = coords; //lat lng
+    this.distance = distance; //in km
+    this.duration = duration; //in min
+  }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+
+  }
+
+  click(){
+    this.clicks++
+  }
+}
+
+//CHILDREN CLASSES
+class Running extends Workout {
+  type = 'running';
+  // CONSTRUCTOR
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+
+    this._setDescription();
+  }
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+  // CONSTRUCTOR
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+    this._setDescription();
+  }
+  calcSpeed() {
+    //min/km
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+/////////////////////////////////////////
+//☁️ REFACTORING FOR PROJECT ARCHITECTURE
+class App {
+  //PRIVATE VALUES
+  #map;
+  #mapZoomLevel = 13;
+  #mapEvent;
+  #workouts = [];
+
+  // CONSTRUCTOR
+  constructor() {
+    //CALLING THE METHODS SO IT SHOWS AT THE START OF LOADING
+
+    this._getPossition();
+    //MARKER APPEARS WHEN YOU CLICK ENTER
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    //☁️ CREATING A TOGGLE FR CYCLING AND RUNNING
+    inputType.addEventListener('change', this._toggleElevationField);
+
+    //WE USE EVENT DELEGATION TO ADD EVENT LISTENERS TO ELEMENTS THAT HAVNT BEEN CREATED
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
+
+    //using local storage to display saved workouts
+    this._getLocalStorage()
+  }
+
+  _getPossition() {
+    //☁️ GETTING THE USERS LOCATION
+    if (navigator.geolocation)
+      //FINDS OUR GEO LOCATION
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get location');
+        }
+      );
+  }
+
+  _loadMap(position) {
+    //THE GEOLOCATION POSITION IS AN OBJECT SO WE TAKE LATITUDE AND LONGITUDE OUT
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // THEN WE INSERT THOSE COORDINATES INTO A GOOGLE MAPS LINK TO SEE IF IT SHOWS OUR LOCATION
+    const coords = [latitude, longitude];
+    // USED LEAFLETS OPEN  SOURCE LIBRARY TO DISPLAY A MAP AT SPECIFIC COORDS
+    //HAD TO ALSO AD THE LEAFLET SCRIPTS TO THE HTML
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work=> this._renderWorkoutMarker(work))
+
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    //☁️ FORM APPEARS WHEN YOU CLICK ON THE MAP
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _hideForm() {
+    this._clearFields();
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000);
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(event) {
+    event.preventDefault();
+
+    //SMALL HELPER Functions
+    //Check if data is valid
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    //Get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    //if workout is running create running object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+    //if workout is cycling create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers');
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    //add new object to workout array
+    this.#workouts.push(workout);
+
+    //render workout on map as marker'
+    this._renderWorkoutMarker(workout);
+
+    //render workout on list
+    this._renderWorkout(workout);
+    // hide formand clear input fields
+    this._hideForm();
+
+    // set local storage to all workouts
+    this._setLocalStorage();
+  }
+
+  _renderWorkoutMarker(workout) {
+    //☁️ MOVING MARKER ON CLICK
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+      )
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`
+      )
+      .openPopup();
+  }
+
+  _renderWorkout(workout) {
+    let html = `
+    <li class="workout workout--${workout.type}" data-id="${workout.id}">
+      <h2 class="workout__title">${workout.description}</h2>
+      <div class="workout__details">
+        <span class="workout__icon">${
+          workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+        }</span>
+        <span class="workout__value">${workout.distance}</span>
+        <span class="workout__unit">km</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">⏱</span>
+        <span class="workout__value">${workout.duration}</span>
+        <span class="workout__unit">min</span>
+      </div>
+          `;
+
+    if (workout.type === 'running')
+      html += `
+      <div class="workout__details">
+        <span class="workout__icon">⚡️</span>
+        <span class="workout__value">${workout.pace.toFixed(1)}</span>
+        <span class="workout__unit">min/km</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">🦶🏼</span>
+        <span class="workout__value">${workout.cadence}</span>
+        <span class="workout__unit">spm</span>
+      </div>
+    </li>
+    `;
+
+    if (workout.type === 'cycling')
+      html += `
+      <div class="workout__details">
+        <span class="workout__icon">⚡️</span>
+        <span class="workout__value">${workout.speed.toFixed(1)}</span>
+        <span class="workout__unit">km/h</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">⛰</span>
+        <span class="workout__value">${workout.elevationGain}</span>
+        <span class="workout__unit">m</span>
+      </div>
+    </li> 
+    `;
+
+    form.insertAdjacentHTML('afterend', html);
+  }
+
+  _clearFields() {
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+  }
+
+  _moveToPopup(event) {
+    const workoutEl = event.target.closest('.workout');
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel + 1, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+    //WHEN WORKING WOTH LOCAL STORAGE THE OBJECTS DONT INHERIT Functions
+    // workout.click();
+  }
+
+  _setLocalStorage(){
+    localStorage.setItem('workouts',JSON.stringify(this.#workouts))
+  }
+  _getLocalStorage(){
+  const data = JSON.parse(localStorage.getItem('workouts'))
+  if (!data) return;
+
+  this.#workouts = data;
+  this.#workouts.forEach(work=> this._renderWorkout(work))
+
+  }
+  reset(){
+    localStorage.removeItem('workouts');
+    location.reload();
+  }
+}
+//WE CALL THE CLASS IN THE CONSTRUCTOR BUT NEED TO DECLARE IT OUTSIDE OF THE CLASS FIRST
+const app = new App();
--- Chapter 4 Javascript/complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ Chapter 4 Javascript/complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,194 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+//https://restcountries.com/v2/
+
+//THE OLD WAY TO DO AJAX CALLS
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+            </div>
+            </article>
+            `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+};
+
+// const getCountryAndNeighbour = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     //RETURNS A JSON OF DATA
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     //render country 1
+//     renderCountry(data);
+
+//     //neighbor country 2
+//     const [neighbor] = data.borders
+//     console.log(neighbor);
+
+//     if (!neighbor) return;
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbor}`);
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+
+//     const data2 = JSON.parse(this.responseText);
+//     console.log(data2);
+//     renderCountry(data2, 'neighbour')
+//     })
+
+//   });
+// };
+
+// getCountryAndNeighbour('germany')
+// getCountryAndNeighbour('Guatemala')
+// getCountryAndNeighbour('France')
+// getCountryAndNeighbour('portugal')
+
+//PROMISSES
+
+// const request = fetch('https://restcountries.com/v2/name/usa');
+// console.log(request);
+
+// //simplifying the API request
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`).then(function (
+//     response
+//   ) {
+//     console.log(response);
+//    return response.json();
+//   }).then(function(data){
+//     console.log(data);
+//     renderCountry(data[0])
+//   });
+// };
+
+//EVEN SIMPILER
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(response => response.json()
+//         // console.log(response.json())
+//     // if (!response.ok)
+//     // throw new Error(`Country not found (${response.status})`)
+//     )
+//     // .catch(err =>console.error([`${err} HELP MEEEEEEE`]))
+
+//     .then(data => {
+//       renderCountry(data[0]);
+//       console.log(...data);
+//       const allNeighbors = data[0].borders;
+//       // I wanted to find all neighbouring countries at once
+//       if (!allNeighbors) return;
+//       allNeighbors.forEach(neighbor => {
+//         console.log(neighbor);
+
+//         //ALWAYS RETURN BEFORE ANOTHER CHAIN
+//         return fetch(`https://restcountries.com/v2/alpha/${neighbor}`)
+//         .then(response => response.json())
+//         .then(data =>renderCountry(data,'neighbour'));
+          
+//         })
+//     //   const neighbor = data[0].borders[0];
+
+
+//     }).catch(err =>{console.error([`${err} HELP MEEEEEEE`]);
+//     renderError(`SOMETHING IS WRONG  ${err.message} HELP MEEEEEEE`)
+// })
+// .finally(()=>{
+//     countriesContainer.style.opacity = 1;
+//     console.log(`HI HI HI`);
+// })
+// };
+// const getJSON = function (url) {
+//     return fetch(url)
+//     .then(response => response.json())  
+
+// }
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+    return fetch(url).then(response => {
+      if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+  
+      return response.json();
+    });
+  };
+  
+
+const getCountryData = function (country) {
+      getJSON(`https://restcountries.com/v2/name/${country}`)
+      .then(data => {
+        renderCountry(data[0]);
+        const neighbor = data[0].borders[0];
+
+        if(!neighbor) throw new Error('No neighbor found!')
+          //ALWAYS RETURN BEFORE ANOTHER CHAIN
+          return getJSON(`https://restcountries.com/v2/alpha/${neighbor}`)
+          .then(data =>renderCountry(data,'neighbour'));
+            
+  
+  
+      }).catch(err =>{console.error([`${err} HELP MEEEEEEE`]);
+      renderError(`SOMETHING IS WRONG  ${err.message} HELP MEEEEEEE`)
+  })
+  .finally(()=>{
+      countriesContainer.style.opacity = 1;
+      console.log(`HI HI HI`);
+  })
+  };
+  
+
+//   const getCountryData = function (country) {
+//     // Country 1
+//     getJSON(
+//       `https://restcountries.eu/rest/v2/name/${country}`,
+//       'Country not found'
+//     )
+//       .then(data => {
+//         renderCountry(data[0]);
+//         const neighbour = data[0].borders[0];
+  
+//         if (!neighbour) throw new Error('No neighbour found!');
+  
+//         // Country 2
+//         return getJSON(
+//           `https://restcountries.eu/rest/v2/alpha/${neighbour}`,
+//           'Country not found'
+//         );
+//       })
+  
+//       .then(data => renderCountry(data, 'neighbour'))
+//       .catch(err => {
+//         console.error(`${err} 💥💥💥`);
+//         renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+//       })
+//       .finally(() => {
+//         countriesContainer.style.opacity = 1;
+//       });
+//   };
+  
+btn.addEventListener('click', function () {
+    getCountryData('germany');
+    
+})
+
+const renderError = function (message) {
+    countriesContainer.insertAdjacentText('beforeend',message);
+    // countriesContainer.style.opacity = 1;
+}
+// getCountryData('gigi')
